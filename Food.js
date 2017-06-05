e770800e4ca9232dc49cdb2def17e7f0c2956b4a
Leonardo <leonardooc@globo.com>
2017-06-05 13:02:14 -0300

    Adição de funcionalidades
    Há gameover com score. Novo jogo. E a criação de "comida" em local aleatório não ocupado pela Snake

--- Food.js
+++ Food.js
@@ -1,5 +1,52 @@
 function Food(){
-	this.generate = function(snake){ //Melhorar, nao precisar receber o snake todo;
-		
+
+	this.active_food = false;
+	this.food_position = (0,0);
+	this.size = 10;
+	this.randomx = ((width-this.size)/this.size);
+	this.randomy = ((height-this.size)/this.size);
+	this.score = 0;
+	
+	this.setSize = function(tamanho){
+		this.size=tamanho;
+	}
+
+	this.show = function(){
+		fill(255,0,0);
+		rect(this.food_position.x,this.food_position.y,this.size,this.size);
+	}
+
+	this.generate = function(snake_positions){
+		var match = true;		
+
+		if(!this.active_food){
+			while(match){
+				match=false;
+				this.food_position = createVector(floor(random(1,this.randomx))*this.size,floor(random(1,this.randomy))*this.size);
+				for(var i=0;i<snake_positions.length;i++){
+					if(snake_positions[i].x==this.food_position.x && snake_positions[i].y==this.food_position.y){
+						match=true;
+						break;
+					}
+				}
+			}
+		}
+		this.active_food = true;
 	}	
+
+	this.eated = function(){
+		this.active_food = false;
+		this.score++;
+	}
+
+	this.getPosition = function(){
+		return(this.food_position);}
+
+	this.getScore = function(){
+		return(this.score);}
+
+	this.resetScore = function(){
+		this.score=0;
+	}
+
 }
